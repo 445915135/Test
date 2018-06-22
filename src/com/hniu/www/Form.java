eee1e1533290339f78451217088ca2c9257afc2c
445915135 <445915135@qq.com>
2018-06-22 14:24:17 +0800

    1111

---
+++ src/com/hniu/www/Form.java
@@ -0,0 +1,20 @@
+package com.hniu.www;
+
+public class Form {
+    public String userName;
+           String password;
+ 
+		public String getuserName() {
+			return userName;
+		}
+		public void setuserName(String userName) {
+			this.userName= userName;
+		}
+		public String getpassword() {
+			return password;
+		}
+		public void setpassword(String password) {
+			this.password = password;
+		}
+           
+}
